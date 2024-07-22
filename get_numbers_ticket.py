af32b7c18893fa83384723da7e6cc38ea610551c
Olga Sekretaryova <sekret.o2a@gmail.com>
2024-07-22 23:01:53 +0300

    fix: error handling (return empty list)

--- get_numbers_ticket.py
+++ get_numbers_ticket.py
@@ -1,23 +1,18 @@
 import random
 
 def get_numbers_ticket(min, max, quantity):
+    empty_list = list()
     # check arguments
-    if min < 1: 
-        raise ValueError('Minimal limit should be equal or more than 1') 
-    if max > 1000:
-        raise ValueError('Maximum limit should be equal or less than 1000')
-    if max <= min:
-        raise ValueError(f'Maximum limit should more than {min}')
-    if quantity < 1:
-        raise ValueError('Minimal limit should be equal or more than 1') 
-    
+    if min < 1 or max > 1000 or max <= min or quantity < 1: 
+        return empty_list
+
     # generate list of possible numbers
     possible_numbers = list(range(min, max + 1, 1))
 
     # check if quantity not more than possible numbers
     quantity_limit = len(possible_numbers)
     if quantity > quantity_limit:
-        raise ValueError(f'Quantity can`t be more than {quantity_limit}') 
+        return empty_list
 
     winning_numbers = random.sample(possible_numbers, quantity)
     winning_numbers.sort()
